0a82d451b7c8529f6ae45376d10cb21ae0bf37b2
Gabriel Gamosi Silva <gamosi.dev@gmail.com>
2023-04-19 17:33:22 -0300

    Base do layout criado e algumas cores alteradas

--- lib/themes/base_theme.dart
+++ lib/themes/base_theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 ThemeData baseTheme = ThemeData(
-    primaryColor: Colors.lightBlue,
-    colorScheme: ColorScheme.fromSwatch(
-      primarySwatch: Colors.lightBlue,
-    ));
+  primaryColor: Colors.lightBlue,
+  backgroundColor: Color.fromRGBO(40, 98, 128, 0.5),
+  colorScheme: ColorScheme.fromSwatch(
+    primarySwatch: Colors.lightBlue,
+  ),
+);
---
+++ lib/view/main_view.dart
@@ -0,0 +1,22 @@
+import 'package:climatempo/themes/base_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: baseTheme.backgroundColor,
+      body: Container(),
+    );
+  }
+}
