8e427bf233da26e948775703a16362302b10c4a3
Gabriel Gamosi Silva <gamosi.dev@gmail.com>
2023-05-26 18:46:34 -0300

    remoção de imports

--- lib/view/show_data_search_view.dart
+++ lib/view/show_data_search_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:weatherreport/model/city_model.dart';
 
 class ShowDataSearchView extends StatelessWidget {
